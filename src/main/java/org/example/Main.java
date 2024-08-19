d17d3d74cbea7b18f6b44c71db45ce66b24e90d8
negin <negin.shekari75@gmail.com>
2024-08-19 12:40:59 +0330

    committed

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warning message");
+        logger.error("This is an error message");
+        logger.fatal("This is a fatal message");
+
+        System.out.println("Running fat jar!");
+    }
+}
