c06c22fe89a7cd1a1a38c3c315e830f1dc7814a5
negin <negin.shekari75@gmail.com>
2024-08-19 13:49:34 +0330

    upgrade log4j

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         logger.debug("This is a debug message");
         logger.info("This is an info message");
